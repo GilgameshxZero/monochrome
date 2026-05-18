1993e9d06727c97b379c6a833bb2f620d2c5d827
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2026-05-18 07:47:28 +0000

    epr, mpv, ignore windows-library

---
+++ bin/mpv.bat
@@ -0,0 +1,2 @@
+@ECHO OFF
+CALL  %~dp0/../app/mpv/mpv.exe %*
--- bin/ytdl.bat
+++ bin/ytdl.bat
@@ -1,3 +1,3 @@
 @ECHO OFF
 @REM Proxy for `yt-dlp` with some defaults.
-CALL  %~dp0/../app/yt-dlp.exe --cookies-from-browser chromium --js-runtimes node --remote-components ejs:github %*
+CALL  %~dp0/../app/yt-dlp.exe --cookies-from-browser chromium --js-runtimes node --remote-components ejs:github -f "bv[height>=720][filesize<1G] + ba[filesize < 100M] / wv[height>=720] + wa[filesize >= 100M] / wv + wa" %*
