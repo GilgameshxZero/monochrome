aeb965aaa757f75abc9ed4642094053b1d5d8262
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2026-06-18 15:29:28 +0000

    consolidate user scripts under gx namespace

--- machine/gilgamesh-51/reset_radio.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-sleep 0.1
-sudo modprobe -r btintel_pcie
-sudo modprobe -r iwlmld
-sudo modprobe -r iwlwifi
-sleep 0.1
-sudo modprobe iwlwifi
-sudo modprobe iwlmld
-sudo modprobe btintel_pcie
