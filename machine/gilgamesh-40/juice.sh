f34bc40400b6679ffc7c419867ac324019a5808e
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2026-06-19 15:13:16 +0000

    tunnel

--- machine/gilgamesh-40/juice.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-# Doesn't work yet.
-/root/juice/juice agent run e6a6ad00-fee6-4a5a-acb0-8ae178ef965e
--- machine/gilgamesh-45/tunnel.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env bash
-ssh -N -R :60045:127.0.0.1:22 -R :61045:127.0.0.1:1194 -o ExitOnForwardFailure=yes -o ServerAliveInterval=60 guest@gilgamesh.cc
