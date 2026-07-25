d7bacad09b6b021eb3466b6e1ffd7aa3baadd92e
gilgamesh <c706213d-56d4-42c1-a681-27fd4135a504@gilgamesh.cc>
2026-07-25 15:42:21 +0000

    tuning

--- machine/gilgamesh-40/start.sh
+++ machine/gilgamesh-40/start.sh
@@ -7,14 +7,12 @@
 qm start 141
 qm start 142
 qm start 143
-# qm start 144
-qm start 145
 qm start 147
 qm start 148
 qm start 149
 
 # VMs which need unlock are specified here.
-sleep 16
+sleep 64
 ./sendkeys.sh 142 $(cat /mnt/pve/sdb1/import/sendkeys.142.txt.raw)
 ./sendkeys.sh 143 $(cat /mnt/pve/sdb1/import/sendkeys.143.txt.raw)
 ./sendkeys.sh 147 $(cat /mnt/pve/sdb1/import/sendkeys.147.txt.raw)
